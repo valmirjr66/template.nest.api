260787fa6c202e6e9230a63864e023c1c58876cd
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2023-12-23 21:53:46 -0300

    Implement controller exceptions treatment

--- src/controller/TextController.ts
+++ src/controller/TextController.ts
@@ -1,26 +1,41 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import GetTextResponseDto from 'src/dto/GetTextResponseDto';
 import InsertTextRequestDto from 'src/dto/InsertTextRequestDto';
 import InsertTextResponseDto from 'src/dto/InsertTextResponseDto';
 import TextService from 'src/service/TextService';
+import BaseController from './BaseController';
 
 @ApiTags('Text')
 @Controller('texts')
-export default class TextController {
-  constructor(private readonly textService: TextService) {}
+export default class TextController extends BaseController {
+  constructor(private readonly textService: TextService) {
+    super();
+  }
 
   @Get(':id')
-  async getById(id: string): Promise<GetTextResponseDto> {
-    return await this.textService.getById(id);
+  @ApiNotFoundResponse({ description: 'Not found' })
+  async getById(@Param('id') id: string): Promise<GetTextResponseDto> {
+    const response = await this.textService.getById(id);
+    this.validateGetResponse(response);
+    return response;
   }
 
   @Get()
+  @ApiNoContentResponse({ description: 'No content' })
   async getAll(): Promise<GetTextResponseDto[]> {
-    return await this.textService.getAll();
+    const response = await this.textService.getAll();
+    this.validateGetResponse(response);
+    return response;
   }
 
   @Post()
+  @ApiBadRequestResponse({ description: 'Bad request' })
   async insert(
     @Body() text: InsertTextRequestDto,
   ): Promise<InsertTextResponseDto> {
