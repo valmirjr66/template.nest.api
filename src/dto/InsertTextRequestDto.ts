9bbe2eb383377b537c77d1d71c7d53d181187d49
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2024-03-13 17:25:59 -0300

    Implements InsertTextRequestDto constructor

--- src/dto/InsertTextRequestDto.ts
+++ src/dto/InsertTextRequestDto.ts
@@ -1,6 +1,11 @@
 import { MaxLength } from 'class-validator';
 
 export default class InsertTextRequestDto {
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
   @MaxLength(50)
   title: string;
 
