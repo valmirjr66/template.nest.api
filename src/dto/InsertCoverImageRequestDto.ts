77281b74a0cc35eea91561283e8b06f2f19be3bf
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2024-03-14 14:04:36 -0300

    Implement constructors

--- src/dto/InsertCoverImageRequestDto.ts
+++ src/dto/InsertCoverImageRequestDto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export default class InsertCoverImageRequestDto {
+  constructor(coverImage: Express.Multer.File) {
+    this.coverImage = coverImage;
+  }
+
   @ApiProperty({ type: 'string', format: 'binary' })
   coverImage: Express.Multer.File;
 }
