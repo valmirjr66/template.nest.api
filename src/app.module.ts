62805e83df8016772a1dd274f9da6aaabfbe90b2
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2024-03-20 19:43:41 -0300

    Add configuration module to read .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import BlobManager from './blob/BlobManager';
 import TextController from './controller/TextController';
 import TextRepository from './repository/TextRepository';
 import TextService from './service/TextService';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [TextController],
   providers: [TextService, TextRepository, AttachmentRepository, BlobManager],
 })
