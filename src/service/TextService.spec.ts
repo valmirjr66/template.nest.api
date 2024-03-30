3a04d13ece2bf2fba9968ed0c4314d3bba0dbc12
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2024-03-30 16:17:21 -0300

    Media file attachment new test

--- src/service/TextService.spec.ts
+++ src/service/TextService.spec.ts
@@ -1,17 +1,35 @@
 import BlobManager from 'blob/BlobManager';
+import InsertAttachmentRequestModel from 'model/InsertAttachmentRequestModel';
 import DataManagerFactory from 'repository/DataManagerFactory';
 import { v4 as uuidv4 } from 'uuid';
+import AttachmentRepository from '../repository/AttachmentRepository';
 import TextRepository from '../repository/TextRepository';
 import TextService from './TextService';
 
+const mockedTextRepo = jest.mocked(TextRepository);
+const mockedAttachmentRepo = jest.mocked(AttachmentRepository);
+const mockedBlobManager = jest.mocked(BlobManager);
+
 jest.mock('../repository/TextRepository');
 jest.mock('../repository/AttachmentRepository');
-
-const mockedTextRepo = jest.mocked(TextRepository);
+jest.mock('../blob/BlobManager');
+jest.mock('../repository/TransactionalDataManager', () => {
+  return {
+    default: jest.fn().mockImplementation(() => {
+      return {
+        beginTransaction: () => {},
+        commitTransaction: () => {},
+        rollbackTransaction: () => {},
+        textRepository: mockedTextRepo.prototype,
+        attachmentRepository: mockedAttachmentRepo.prototype,
+      };
+    }),
+  };
+});
 
 describe('Text Service', () => {
   const textService = new TextService(
-    new BlobManager(),
+    mockedBlobManager.prototype,
     new DataManagerFactory(),
   );
 
@@ -27,10 +45,21 @@ describe('Text Service', () => {
     attachments: [],
   };
 
+  const SAMPLE_ATTACHMENT_MODEL = {
+    id: uuidv4(),
+    fileExtension: 'png',
+    publicationDate: new Date(),
+    text: SAMPLE_TEXT_MODEL,
+    textId: SAMPLE_TEXT_MODEL.id,
+  };
+
   beforeAll(() => {
     mockedTextRepo.prototype.findById.mockResolvedValue(SAMPLE_TEXT_MODEL);
     mockedTextRepo.prototype.findAll.mockResolvedValue([SAMPLE_TEXT_MODEL]);
     mockedTextRepo.prototype.save.mockResolvedValue(SAMPLE_TEXT_MODEL);
+    mockedAttachmentRepo.prototype.save.mockResolvedValue(
+      SAMPLE_ATTACHMENT_MODEL,
+    );
   });
 
   it('Should return the sample text', async () => {
@@ -55,5 +84,32 @@ describe('Text Service', () => {
     });
 
     expect(result).toStrictEqual(SAMPLE_TEXT_MODEL);
+  });
+
+  it('Should successfuly attach a new media file', async () => {
+    const randomTextId = uuidv4();
+
+    const mockedMedia = <Express.Multer.File>{
+      originalname: 'file.png',
+      buffer: Buffer.from('abc'),
+    };
+
+    const result = await textService.attachMedia({
+      textId: randomTextId,
+      media: mockedMedia,
+    });
+
+    const attachment = new InsertAttachmentRequestModel(
+      expect.any(String),
+      'png',
+      expect.any(Date),
+      randomTextId,
+    );
+
+    expect(mockedAttachmentRepo.prototype.save).toHaveBeenCalledWith(
+      attachment,
+    );
+
+    expect(result).toStrictEqual(SAMPLE_ATTACHMENT_MODEL);
   });
 });
