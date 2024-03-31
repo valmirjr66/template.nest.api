db8e73ec5d3f29d4b2d725c8b0e03211ac36e247
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2024-03-31 16:58:15 -0300

    Remove migrations from coverage

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     '**/*.(t|j)s',
     '!app.module.ts',
     '!main.ts',
-    '!**/repository/*.(t|j)s',
+    '!**/repository/**/*.(t|j)s',
     '!**/constants/*.(t|j)s',
     '!**/model/*.(t|j)s',
   ],
